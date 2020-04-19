9d0ee1ad4260220059d83e0e79d7ebc12a1ec01b
LiuChen <strangejugg@hotmail.com>
2020-04-19 17:47:30 +0800

    upd: springboot project init

---
+++ server/src/main/java/cn/edu/buaa/se/bhac/BhacApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.buaa.se.bhac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhacApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/cn/edu/buaa/se/bhac/BhacApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.buaa.se.bhac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
