cec0636134b5ef7bc84cfe326a1759844e0e90c7
Backkom <1749407479@qq.com>
2020-04-30 19:09:49 +0800

    add sql folder

---
+++ bhac/src/main/resources/sql/bhac_actUserRole.sql
@@ -0,0 +1,16 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 8.0.18 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `bhac_actuserrole` (
+	`id` int (11),
+	`uid` int (11),
+	`rid` int (11)
+); 
+insert into `bhac_actuserrole` (`id`, `uid`, `rid`) values('1','1','4');
+insert into `bhac_actuserrole` (`id`, `uid`, `rid`) values('2','2','4');
+insert into `bhac_actuserrole` (`id`, `uid`, `rid`) values('3','1','5');
+insert into `bhac_actuserrole` (`id`, `uid`, `rid`) values('4','3','4');
