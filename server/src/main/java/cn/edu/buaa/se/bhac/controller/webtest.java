1d0aeb8cd05334d4509dd1cb843aeb901674e1f2
nwkun <474855890@qq.com>
2020-04-30 10:23:58 +0800

    changebacktowk

--- server/src/main/java/cn/edu/buaa/se/bhac/controller/webtest.java
+++ server/src/main/java/cn/edu/buaa/se/bhac/controller/webtest.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class webtest {
 
     @GetMapping("helloworld")
-    public String test() {
+    public String test1() {
         return "helloworld";
+    }
+
+    @GetMapping("login")
+    public String test2() {
+        return "login";
+    }
+
+    @GetMapping("index")
+    public String test3() {
+        return "index";
     }
 }
