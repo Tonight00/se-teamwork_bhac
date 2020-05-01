531b04c089c0ef7a9f4682eb2409643cb50d16a2
Backkom <1749407479@qq.com>
2020-05-01 08:43:01 +0800

    add sql foler

---
+++ server/src/main/resources/sql/bhac_belongActivityTag.sql
@@ -0,0 +1,16 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 8.0.18 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `bhac_belongactivitytag` (
+	`id` int (11),
+	`aid` int (11),
+	`tid` int (11)
+); 
+insert into `bhac_belongactivitytag` (`id`, `aid`, `tid`) values('1','1','1');
+insert into `bhac_belongactivitytag` (`id`, `aid`, `tid`) values('2','1','2');
+insert into `bhac_belongactivitytag` (`id`, `aid`, `tid`) values('3','2','2');
+insert into `bhac_belongactivitytag` (`id`, `aid`, `tid`) values('4','3','3');
