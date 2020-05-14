a0e7658e4478ff3ab3b8e8a7bec962b6c03a4b42
chenhaonan <949165060@qq.com>
2020-05-14 18:20:11 +0800

    add  favourite search function

---
+++ android app/app/src/main/java/com/shakespace/template/copy/BhacJoinuseractivity.java
@@ -0,0 +1,42 @@
+package com.shakespace.template.copy;
+
+public class BhacJoinuseractivity {
+    private Integer id;
+
+    private Integer uid;
+
+    private Integer aid;
+
+    private Integer state;
+
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getAid() {
+        return aid;
+    }
+    public void setAid(Integer aid) {
+        this.aid = aid;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+
+}
