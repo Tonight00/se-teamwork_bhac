a3378f0f231d354496013ceb4f16cfb5dc967d39
chenhaonan <949165060@qq.com>
2020-05-14 17:47:34 +0800

    321312

---
+++ android app/app/src/main/java/com/shakespace/template/copy/BhacPost.java
@@ -0,0 +1,79 @@
+package com.shakespace.template.copy;
+
+public class BhacPost {
+
+
+    private Integer id;
+
+    private String title;
+    private String content;
+
+    private Integer type;
+
+    private Integer aid;
+
+    private Integer tid;
+
+    private Integer rate;
+
+
+    public Integer getAid ()
+    {
+        return aid;
+    }
+
+    public void setAid (Integer aid)
+    {
+        this.aid = aid;
+    }
+
+    public Integer getTid ()
+    {
+        return tid;
+    }
+
+    public void setTid (Integer tid)
+    {
+        this.tid = tid;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+
+
+
+    public Integer getType() {
+        return type;
+    }
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+
+    public Integer getRate() {
+        return rate;
+    }
+
+    public void setRate(Integer rate) {
+        this.rate = rate;
+    }
+
+
+
+
+
+}
